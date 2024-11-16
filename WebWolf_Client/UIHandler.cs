cf1f38e230d9117f598f9e7f62a99ed7d1efad9f
Jannis Edler <jannisedler4@gmail.com>
2024-11-16 15:58:36 +0100

    Added live display that displays all Player in the lobby.

--- WebWolf_Client/UIHandler.cs
+++ WebWolf_Client/UIHandler.cs
@@ -7,6 +7,10 @@ public class UIHandler
 {
     public static bool StartGameMenu()
     {
+        //Test Spierler
+        PlayerManager.Players.Add(new PlayerData("TestSpieler1", null));
+        PlayerManager.Players.Add(new PlayerData("TestSpieler2", "2"));
+        
         Console.Clear();
         ConsoleUtils.RenderLogo();
         
@@ -57,6 +61,23 @@ public class UIHandler
 
     public static void DisplayLobby()
     {
-        // Fehlt noch
+        AnsiConsole.Live(new Markup("[green]Lobby wird geladen...[/]"))
+            .Start(ctx =>
+            {
+                while (true)
+                {
+                    var table = new Table();
+                    table.AddColumn("[blue]Name[/]");
+                    table.AddColumn("[blue]ID[/]");
+                    
+                    foreach (var player in PlayerManager.Players)
+                    {
+                        table.AddRow(player.Name, player.Id ?? "[grey]Keine ID[/]");
+                    }
+                    
+                    table.AddRow($"[bold green]{PlayerData.LocalPlayer.Name}[/]", "[green](You)[/]");
+                    ctx.UpdateTarget(table);
+                }
+            });
     }
 }
